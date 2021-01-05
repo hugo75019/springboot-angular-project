5305be8163da72dc0437a8e51ba224bc94db52bc
Hugo SABABADY <hugosababady@gmail.com>
2021-01-05 15:45:26 +0100

    import du projet (création des différentes classes et méthodes)

---
+++ springbootalbum/src/test/java/com/example/springbootalbum/SpringbootalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
