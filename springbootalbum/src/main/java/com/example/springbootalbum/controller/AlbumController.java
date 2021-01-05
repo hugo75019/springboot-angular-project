fead490a7463672e30278f53896297273910e232
Hugo SABABADY <hugosababady@gmail.com>
2021-01-05 17:58:00 +0100

    ajout methode permettant de supprimer un album selon son id.

--- springbootalbum/src/main/java/com/example/springbootalbum/controller/AlbumController.java
+++ springbootalbum/src/main/java/com/example/springbootalbum/controller/AlbumController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,4 +60,20 @@ public class AlbumController {
 		Album editedAlbum = albumRepository.save(album);
 		return ResponseEntity.ok(editedAlbum);
 	}
+	
+	@DeleteMapping("/albums/{id}")
+	@CrossOrigin(origins = "http://localhost:4200")
+	public boolean deleteAlbum(@PathVariable Long id) {
+		boolean suppr = false ;
+		try {
+			albumRepository.deleteById(id);
+			suppr = true;
+		} catch (Exception e) {
+			suppr = false;
+		}
+		return suppr;
+	}
+	
+	
+	
 }
